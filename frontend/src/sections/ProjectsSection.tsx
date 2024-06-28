6dba1001ee9c97b5fc2523ca7f16a77091e1c627
Blake <gblake321@gmail.com>
2024-06-28 18:07:57 -0400

    Project Links

--- frontend/src/sections/ProjectsSection.tsx
+++ frontend/src/sections/ProjectsSection.tsx
@@ -10,17 +10,16 @@ interface ProjectProps {
     description: string
     imageSrc: string
     visit: string
-    github: string
 }
 
-function Project({title, subtitle, description, imageSrc, visit, github}: ProjectProps) {
+function Project({title, subtitle, description, imageSrc, visit}: ProjectProps) {
     return (
-        <button className = 'projectButton' style = {{position: 'relative', border: '3px solid black', width: '47%', height: '480px', display: 'flex', flexDirection: 'column', borderRadius: '10px', boxShadow: '2px 2px 2px rgb(0, 0, 0, 0.2)'}}>
+        <button className = 'projectButton' onClick = {() => {window.open(visit, '_blank')}} style = {{border: '3px solid black', width: '47%', height: '480px', display: 'flex', flexDirection: 'column', borderRadius: '10px', boxShadow: '2px 2px 2px rgb(0, 0, 0, 0.2)'}}>
             <div style = {{width: '100%', height: '70%', backgroundImage: 'url(' + imageSrc + ')', backgroundSize: '100%', backgroundPositionY: '-2px'}}>
             </div>
-            <div style = {{borderTop: '1px solid black', padding: '10px'}}>
-                <h4><i><strong>{title}</strong></i></h4>
-                <p>{subtitle}</p>
+            <div style = {{display: 'flex', flexDirection: 'column', borderTop: '1px solid black', padding: '10px'}}>
+                <h3 style = {{alignSelf: 'flex-start'}}><i><strong>{title}</strong></i></h3>
+                <p style = {{textAlign: 'left'}}>{subtitle}</p>
             </div>
         </button>
     )
@@ -33,7 +32,7 @@ function ProjectsSection() {
         <h2 style = {{marginBottom: '50px'}}>My Projects</h2>
         <div style = {{display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between', gap: '50px'}}>
             {
-                projectsList.map((prj) => <Project title = {prj.name} subtitle={prj.subtitle} description={prj.description} imageSrc={prj.imgSrc} visit = {prj.visit} github={prj.github}/>)
+                projectsList.map((prj) => <Project title = {prj.name} subtitle={prj.subtitle} description={prj.description} imageSrc={prj.imgSrc} visit = {prj.visit}/>)
             }
         </div>
     </div>
